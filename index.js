4d8f60628591f38dd8e7bd3a6137143d86ba1243
Nikhil <nikhild12377@gmail.com>
2022-04-18 16:22:25 +0530

    new changes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+$(".owl-carousel").owlCarousel({
+  center: true,
+  items: 2,
+  loop: true,
+  margin: 10,
+  responsive: {
+    600: {
+      items: 4,
+    },
+  },
+});
+
 document.querySelector(".form").addEventListener("submit", async (event) => {
   event.preventDefault();
   try {
